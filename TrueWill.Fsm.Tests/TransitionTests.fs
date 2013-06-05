637106cb1c948c65a0d396f0118c729c2521c3c9
Bill Sorensen <bill@truewill.net>
2013-06-04 21:41:39 -0500

    Updated project to Visual Studio 2012. Added Visual Studio solution file. Added a basic unit test project using FsUnit.xUnit (via NuGet).

---
+++ TrueWill.Fsm.Tests/TransitionTests.fs
@@ -0,0 +1,11 @@
+﻿module TransitionTests
+
+open TrueWill.Fsm
+open Xunit
+open FsUnit.Xunit
+
+[<Fact>]
+let toString_WhenCalled_ReturnsText () =
+    let transition = { CurrentState = "Current"; TriggeringEvent = "event"; NewState = "New" }
+    let s = transition.AsString()
+    s |> should equal "Current|event|New"
