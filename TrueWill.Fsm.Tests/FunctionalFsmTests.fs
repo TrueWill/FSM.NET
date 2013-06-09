eb956dfaa82137f4bf0db3acb2cdd323d491d445
Bill Sorensen <bill@truewill.net>
2013-06-09 10:41:54 -0500

    Added test for initial state when no transitions.

---
+++ TrueWill.Fsm.Tests/FunctionalFsmTests.fs
@@ -0,0 +1,9 @@
+﻿module FunctionalFsmTests
+
+open TrueWill.Fsm
+open Xunit
+open FsUnit.Xunit
+
+[<Fact>]
+let getInitialState_WhenNoTransitions_ThrowsArgumentException () =
+    (fun () -> Fsm.getInitialState Seq.empty |> ignore) |> should throw typeof<System.ArgumentException>
