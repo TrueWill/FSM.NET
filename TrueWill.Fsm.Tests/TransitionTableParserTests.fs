ad65084ae8e7b14d3b72fe64d6c11e7e7d16f471
Bill Sorensen <bill@truewill.net>
2013-06-13 22:15:14 -0500

    Added basic parser.
    This is a very crude parser for the transition table DSL
    in order to get the initial test to pass.
    It needs significantly more testing, error checking, etc.

---
+++ TrueWill.Fsm.Tests/TransitionTableParserTests.fs
@@ -0,0 +1,19 @@
+﻿module TransitionTableParserTests
+
+open TrueWill.Fsm
+open Xunit
+open FsUnit.Xunit
+
+[<Fact>]
+let parse_WhenBasic_ReturnsCollection () =
+    let tableText =
+        "Locked|coin|Unlocked\r\n\
+         Unlocked|coin|Unlocked\r\n\
+         Unlocked|pass|Locked"
+
+    let table = TransitionTableParser.parse(tableText)
+
+    Seq.toList table |> should equal
+        [ { CurrentState = "Locked";   TriggeringEvent = "coin"; NewState = "Unlocked" };
+          { CurrentState = "Unlocked"; TriggeringEvent = "coin"; NewState = "Unlocked" };
+          { CurrentState = "Unlocked"; TriggeringEvent = "pass"; NewState = "Locked" } ]
---
+++ TrueWill.Fsm/TransitionTableParser.fs
@@ -0,0 +1,15 @@
+﻿module TrueWill.Fsm.TransitionTableParser
+
+open System.Text.RegularExpressions
+
+// TODO Not using constant for delimiter - need to escape
+// TODO Error checking
+// TODO Allow whitespace (so can line up delimiters if desired)
+// TODO Allow comments and blank lines - maybe just filter those lines?
+// TODO Cleanup
+let parse tableText =
+    let lines = Regex.Split(tableText, @"\r?\n")
+    lines |> Seq.map
+        (fun line ->
+           let mtch = Regex.Match(line, "^(?<currentState>\w+)\|(?<triggeringEvent>\w+)\|(?<newState>\w+)$")
+           { CurrentState = mtch.Groups.["currentState"].Value; TriggeringEvent = mtch.Groups.["triggeringEvent"].Value; NewState = mtch.Groups.["newState"].Value } )
