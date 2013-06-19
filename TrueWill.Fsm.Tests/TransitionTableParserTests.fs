b28492a0a0972e1513e372afe025067aac95e03a
Bill Sorensen <bill@truewill.net>
2013-06-18 21:16:21 -0500

    Checks for events differing only by case.
    Also added more tests.

--- TrueWill.Fsm.Tests/TransitionTableParserTests.fs
+++ TrueWill.Fsm.Tests/TransitionTableParserTests.fs
@@ -1,5 +1,6 @@
 ﻿module TransitionTableParserTests
 
+open System
 open TrueWill.Fsm
 open Xunit
 open FsUnit.Xunit
@@ -32,3 +33,48 @@ let parse_WhenBlankLines_Ignores () =
     Seq.toList table |> should equal
         [ { CurrentState = "Locked";   TriggeringEvent = "coin"; NewState = "Unlocked" };
           { CurrentState = "Unlocked"; TriggeringEvent = "pass"; NewState = "Locked" } ]
+
+[<Fact>]
+let parse_WhenEmpty_ReturnsEmptyCollection () =
+    // Validating this case is not the parser's responsibility.
+    let table = TransitionTableParser.parse(String.Empty)
+    table |> Seq.isEmpty |> should be True
+
+[<Fact>]
+let parse_WhenEventsDifferOnlyByCase_Throws () =
+    let tableText =
+        "Locked|coin|Unlocked\r\n\
+         Unlocked|Coin|Unlocked\r\n\
+         Unlocked|pass|Locked"
+
+    (fun () -> TransitionTableParser.parse(tableText) |> ignore) |> should throw typeof<ArgumentException>
+
+[<Fact>]
+let differOnlyByCase_WhenEmpty_ReturnsFalse () =
+    let result = TransitionTableParser.differOnlyByCase []
+    result |> should be False
+
+[<Fact>]
+let differOnlyByCase_WhenOne_ReturnsFalse () =
+    let result = TransitionTableParser.differOnlyByCase [ "foo" ]
+    result |> should be False
+
+[<Fact>]
+let differOnlyByCase_WhenDuplicates_ReturnsFalse () =
+    let result = TransitionTableParser.differOnlyByCase [ "foo"; "foo" ]
+    result |> should be False
+
+[<Fact>]
+let differOnlyByCase_WhenDistinct_ReturnsFalse () =
+    let result = TransitionTableParser.differOnlyByCase [ "foo"; "bar" ]
+    result |> should be False
+
+[<Fact>]
+let differOnlyByCase_WhenDiffer_ReturnsTrue () =
+    let result = TransitionTableParser.differOnlyByCase [ "foo"; "FoO" ]
+    result |> should be True
+
+[<Fact>]
+let differOnlyByCase_WhenDifferAndDuplicates_ReturnsTrue () =
+    let result = TransitionTableParser.differOnlyByCase [ "foo"; "bar"; "Foo"; "Bar" ]
+    result |> should be True
--- TrueWill.Fsm/TransitionTableParser.fs
+++ TrueWill.Fsm/TransitionTableParser.fs
@@ -5,6 +5,14 @@ open System.Text.RegularExpressions
 let private isBlank line =
     Regex.IsMatch(line, @"^\s*$")
 
+/// Determines if a sequence (which may contain duplicates) contains
+/// any elements that differ only by case.
+/// Exposed primarily for unit tests.
+let differOnlyByCase (items : seq<string>) =
+    let distinctCount = items |> Seq.distinct |> Seq.length
+    let distinctUpperCount = items |> Seq.distinctBy (fun x -> x.ToUpperInvariant()) |> Seq.length
+    distinctCount <> distinctUpperCount
+
 // TODO Not using constant for delimiter - need to escape
 // TODO Error checking
 // TODO Allow whitespace (so can line up delimiters if desired)
@@ -13,9 +21,17 @@ let private isBlank line =
 // TODO Cleanup
 let parse tableText =
     let lines = Regex.Split(tableText, @"\r?\n")
-    lines
+
+    let result =
+        lines
         |> Seq.filter (not << isBlank)
         |> Seq.map
             (fun line ->
             let mtch = Regex.Match(line, "^(?<currentState>\w+)\|(?<triggeringEvent>\w+)\|(?<newState>\w+)$")
             { CurrentState = mtch.Groups.["currentState"].Value; TriggeringEvent = mtch.Groups.["triggeringEvent"].Value; NewState = mtch.Groups.["newState"].Value } )
+        |> Seq.toList
+
+    if result |> Seq.map (fun x -> x.TriggeringEvent) |> differOnlyByCase then
+        raise <| System.ArgumentException("Some events differ only by case.", "tableText")
+
+    result
