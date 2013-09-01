cb541aa26d0de7537fc2ad854f734f39390ce07a
Bill Sorensen <bill@truewill.net>
2013-08-31 22:10:34 -0500

    Added sample WCF web service.

---
+++ Samples/SampleWcfService/IStateMachineService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+
+namespace SampleWcfService
+{
+    [ServiceContract]
+    public interface IStateMachineService
+    {
+        [OperationContract]
+        string GetInitialState();
+
+        [OperationContract]
+        IEnumerable<string> GetAvailableEvents(string currentState);
+
+        [OperationContract]
+        string GetNewState(string currentState, string triggeringEvent);
+    }
+}
---
+++ Samples/SampleWcfService/StateMachineService.svc.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+using System.Web.Configuration;
+using TrueWill.Fsm;
+
+namespace SampleWcfService
+{
+    public class StateMachineService : IStateMachineService
+    {
+        private static readonly StateMachine _stateMachine;
+
+        static StateMachineService()
+        {
+            // Cheesy initialization - I'd probably use Unity.WCF in production.
+            // Not handling errors.
+
+            // Read state machine from Web.config.
+
+            var config = WebConfigurationManager.OpenWebConfiguration("~");
+            var tableText = config.AppSettings.Settings["transitionTable"].Value;
+            var transitions = TransitionTableParser.Parse(tableText);
+            _stateMachine = new StateMachine(transitions);
+        }
+
+        public string GetInitialState()
+        {
+            return _stateMachine.InitialState;
+        }
+
+        public IEnumerable<string> GetAvailableEvents(string currentState)
+        {
+            try
+            {
+                return _stateMachine.GetAvailableEvents(currentState);
+            }
+            catch (Exception ex)
+            {
+                if (ex is InvalidOperationException || ex is ArgumentNullException)
+                {
+                    throw new FaultException(ex.Message);
+                }
+
+                throw;
+            }
+        }
+
+        public string GetNewState(string currentState, string triggeringEvent)
+        {
+            try
+            {
+                return _stateMachine.GetNewState(currentState, triggeringEvent);
+            }
+            catch (Exception ex)
+            {
+                if (ex is InvalidOperationException || ex is ArgumentNullException)
+                {
+                    throw new FaultException(ex.Message);
+                }
+
+                throw;
+            }
+        }
+    }
+}
